9096f7af36cced797af65c52ff4d1d42bd816463
Jia Xu <jia@unity3d.com>
2019-12-11 15:44:58 -0800

    - Change project menu item name to be more clear

--- Assets/Editor/CreateAssetBundle.cs
+++ Assets/Editor/CreateAssetBundle.cs
@@ -4,25 +4,25 @@ public class CreateAssetBundle
 {
     private static string bundleSavePath = "Assets/StreamingAssets/AssetBundles/";
 
-    [MenuItem("Tools/Build AssetBundlers/Windows", false, 2)]
+    [MenuItem("QA/Build AssetBundlers/Windows", false, 2)]
     static void BuildAssetBundleWindows()
     {
         OnBuildAssetBundle(bundleSavePath + "Windows", BuildTarget.StandaloneWindows);
     }
 
-    [MenuItem("Tools/Build AssetBundlers/OSX", false, 2)]
+    [MenuItem("QA/Build AssetBundlers/OSX", false, 2)]
     static void BuildAssetBundleOSX()
     {
         OnBuildAssetBundle(bundleSavePath + "OSX", BuildTarget.StandaloneOSX);
     }
 
-    [MenuItem("Tools/Build AssetBundlers/Linux", false, 2)]
+    [MenuItem("QA/Build AssetBundlers/Linux", false, 2)]
     static void BuildAssetBundleLinux()
     {
         OnBuildAssetBundle(bundleSavePath + "Linux", BuildTarget.StandaloneLinux64);
     }
 
-    [MenuItem("Tools/Build AssetBundlers/UWP", false, 2)]
+    [MenuItem("QA/Build AssetBundlers/UWP", false, 2)]
     static void BuildAssetBundleUWP()
     {
         OnBuildAssetBundle(bundleSavePath + "UWP", BuildTarget.WSAPlayer);
